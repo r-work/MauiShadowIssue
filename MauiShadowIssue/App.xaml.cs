b29b93155ff9141b40ceaa771d0b9bc5d0acb9f1
r-work <rebin@mitransit.co.uk>
2023-06-29 11:56:00 +0100

    init

---
+++ MauiShadowIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiShadowIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiShadowIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiShadowIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
